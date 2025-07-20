a284678fb990fda4389ebe7e794f5a1b54238662
Ranjan Kushwaha <165882508+ranjan986@users.noreply.github.com>
2025-07-20 11:10:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import Admin from './pages/Admin';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <About />
       <Projects />
       <Contact />
+      <Admin />
     </>
   );
 };
