84f5cdc2bf2365b87e82dde442b90035fbc8421e
Ranjan Kushwaha <165882508+ranjan986@users.noreply.github.com>
2025-07-20 11:17:53 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,11 +19,11 @@ const Navbar = () => {
 
       {/* Menu Items */}
       <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/admin">Admin</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#admin">Admin</a></li>
       </ul>
     </nav>
   );
