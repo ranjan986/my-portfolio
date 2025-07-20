912ee7d67295cb120c9d0b74724b3018883514aa
Ranjan Kushwaha <165882508+ranjan986@users.noreply.github.com>
2025-07-20 15:21:04 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,6 @@ const Navbar = () => {
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
-        <li><a href='#admin'>Admin</a></li>
       </ul>
     </nav>
   );
